c141cebdd9dcca0207ab681f125bdf7ce3ece661
meisl <m.kling@sprax.org>
2019-01-18 16:29:31 +0100

    + first test of glob and symlinks

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+'use strict';
+
+const glob = require('glob');
+
 module.exports.version = require('./package.json').version;
+
+module.exports.scan = function (path) {
+	let entries = glob.sync('**', { cwd: path, mark: true });
+	
+	
+	return entries;
+};
+
--- test/main-test.js
+++ test/main-test.js
@@ -1,10 +1,25 @@
-var expect = require("expect.js"),
+'use strict';
+
+const expect = require("expect.js"),
+	util = require("util"),
 	main = require("../");
 
-describe('main.js', function() {
+describe('main.js', function () {
 
-	it('should get the version', function() {
+	it('should get the version', function () {
 		expect(main.version).to.match(/\d+\.\d+\.\d+/);
 	});
 
-})
+	
+	describe('scan', function () {
+		
+		it('should list all files', function () {
+			let entries = main.scan('test/fixtures');
+			expect(entries).to.be.an(Array);
+			expect(entries).to.have.length(10);
+			//expect(entries).to.be("asdf");
+		});
+		
+	});
+});
+
