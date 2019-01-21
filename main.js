e6ae0cb4b09e58b4db2f57cf667fe01f18ec8a67
meisl <m.kling@sprax.org>
2019-01-21 16:15:22 +0100

    * handle various types of links (to DIR, to FILE, and *broken*) in walkSync

--- main.js
+++ main.js
@@ -1,24 +1,84 @@
 'use strict';
 
-const fs = require('fs');
+const fs = require('fs'),
+	path = require('path');
 
 module.exports.version = require('./package.json').version;
 
-const stepSync = module.exports.stepSync = function (path) {
+const stepSync = module.exports.stepSync = function (dirName) {
 	let entries;
 	
-	entries = fs.readdirSync(path, { withFileTypes: true });
+	entries = fs.readdirSync(dirName);
 	return entries;
 };
 
-const walkSync = module.exports.walkSync = function (path) {
-	let entries = [];
-	try {
-		entries = stepSync(path);
-	} catch (e) {
-		if (e.code != 'EPERM') {
-			throw e;
+const _walkSync = function (start, todo, result) {
+	while (todo.length > 0) {
+		let dirName = todo.pop();
+		let current = path.join(start, dirName);
+		let entries = [];
+		try {
+			entries = stepSync(current);
+		} catch (e) {
+			if (e.code != 'EPERM') {
+				throw e;
+			}
 		}
+		result.folders.push({
+			name: dirName
+		});
+		entries.forEach(e => {
+			let entry = {
+				name: path.join(dirName, e),
+			};
+			let ePath = path.join(current, e);
+			let s = fs.lstatSync(ePath);
+			if (s.isSymbolicLink()) {
+				let linkText = fs.readlinkSync(ePath);
+				let tPath = path.join(current, linkText);
+				entry.target = tPath;
+				let tStat;
+				try {
+					tStat = fs.statSync(tPath);
+					if (tStat.isDirectory()) {
+						entry.type = 'DIR';
+					} else if (tStat.isFile()) {
+						entry.type = 'FILE';
+					}
+				} catch (e) {
+					if (e.code != 'ENOENT') {
+						throw e;
+					}
+					entry.type = 'BROKE'
+				}
+				switch (entry.type) {
+					case 'DIR':
+						result.folders.push(entry);
+						break;
+					case 'FILE':
+					case 'BROKE':
+						result.files.push(entry);
+						break;
+					default:
+				}
+			}
+			if (s.isDirectory()) {
+				let more = path.join(dirName, e);
+				todo.push(more);
+			} else if (s.isSymbolicLink()) {
+				// nothing
+			} else if (s.isFile()) {
+				result.files.push({ name: path.join(dirName, e) });
+			}
+		});
 	}
-	return entries;
+}
+
+const walkSync = module.exports.walkSync = function (dirName) {
+	let result = { start: dirName, files: [], folders: [] };
+	_walkSync(dirName, ['.'], result);
+	result.folders.shift();
+	result.length = result.files.length + result.folders.length;
+	
+	return result;
 };
--- test/walk-test.js
+++ test/walk-test.js
@@ -1,55 +1,123 @@
 'use strict';
 
 const expect = require('expect.js'),
-	util = require('util'),
+	path = require('path'),
 	{ stepSync, walkSync } = require('../');
 
-const path_fix = 'test/fixtures',
-	path_inaccassible = path_fix + '/inaccessible';
+const path_fix = path.join('test', 'fixtures'),
+	path_inaccassible = path.join(path_fix, 'inaccessible'),
+	path_nonexistent = path.join('i', 'do', 'not', 'exist');
 
 describe('stepSync', function () {
 
+	it('on a file should throw', function () {
+		expect(stepSync).withArgs('one.txt').to.throwException(e =>
+			expect(e).to.have.property('code', 'ENOENT')
+		);
+	});
+
 	it('into non-existent dir should throw', function () {
-		expect(stepSync).withArgs('i/do/not/exist').to.throwException(e =>
-			expect(e.code).to.be('ENOENT')
+		expect(walkSync).withArgs(path_nonexistent).to.throwException(e =>
+			expect(e).to.have.property('code', 'ENOENT')
 		);
 	});
 
 	it('into dir with insufficient permissions should throw', function () {
 		expect(stepSync).withArgs(path_inaccassible).to.throwException(e =>
-			expect(e.code).to.be('EPERM')
+			expect(e).to.have.property('code', 'EPERM')
 		);
 	});
 
-	it('should list directory entries on the top level', function () {
-		let entries = stepSync(path_fix);	//	'K:');	//	
+	it('on proper directory should list entries on the top level', function () {
+		let entries = stepSync(path_fix);
+		expect(entries).to.have.length(9);
+	});
+
+	it('on symlinked directory should list entries on the top level', function () {
+		let dirName = path.join(path_fix, 'symlink-to-dir');
+		let entries = stepSync(dirName);
 		expect(entries).to.be.an(Array);
-		expect(entries).to.have.length(8);
+		expect(entries).to.have.length(2);
 	});
 
 });
 
 describe('walkSync', function () {
 
-	it('into non-existent dir should throw', function () {
-		expect(walkSync).withArgs('i/do/not/exist').to.throwException(e =>
-			expect(e.code).to.be('ENOENT')
+	it('on a file should throw', function () {
+		expect(walkSync).withArgs('one.txt').to.throwException(e =>
+			expect(e).to.have.property('code', 'ENOENT')
 		);
 	});
 
-	it('into dir with insufficient permissions should return empty array', function () {
-		let entries = walkSync(path_inaccassible);
-		expect(entries).to.be.an(Array);
-		expect(entries).to.have.length(0);
+	it('into non-existent dir should throw', function () {
+		expect(walkSync).withArgs(path_nonexistent).to.throwException(e =>
+			expect(e).to.have.property('code', 'ENOENT')
+		);
 	});
 
+	it('into dir with insufficient permissions should return empty result', function () {
+		let entries = walkSync(path_inaccassible);
+		expect(entries).to.have.length(0);
+		expect(entries).to.have.property('folders');
+		expect(entries.folders).to.eql([]);
+		expect(entries).to.have.property('files');
+		expect(entries.files).to.eql([]);
+	});
+
+	it('on symlinked directory should list all entries from lower levels', function () {
+		let startDir = path.join(path_fix, 'symlink-to-dir');
+		let entries = walkSync(startDir);
+		expect(entries).to.have.length(2);
+		expect(entries.files).to.have.length(2);
+		let fileNames = entries.files.map(e => e.name);
+		expect(fileNames).to.contain('one.txt');
+		expect(fileNames).to.contain('two.txt');
+		
+	});
 
 	it('should NOT follow symlinked dirs outside the starting directory', function () {
-		let entries = walkSync(path_fix + '/to-outside');
-		expect(entries).to.be.an(Array);
-		expect(entries).to.have.length(1);
-		expect(entries[0]).to.have.property('name', 'symlink-to-outside-dir');
+		let startDir = path.join(path_fix, 'to-outside');
+		let entries = walkSync(startDir);
+		expect(entries).to.have.length(3);
+		expect(entries.files).to.have.length(1);
+		expect(entries.files.map(e => e.name)).to.contain('symlink-to-outside-file');
+		expect(entries.folders).to.have.length(2);
+		let folderNames = entries.folders.map(e => e.name);
+		expect(folderNames).to.contain('symlink-to-outside-dir');
+		expect(folderNames).to.contain('symlink-to-not-so-outside-dir');
 	});
 
+	it('on proper directory should list all entries from lower levels', function () {
+		let entries = walkSync(path_fix);
+		let exp;
+		
+		let folderNames = entries.folders.map(e => e.name);
+		expect(folderNames).to.contain('to-outside');
+		expect(folderNames).to.contain(
+			path.join('to-outside', 'symlink-to-outside-dir'));
+		expect(entries.folders).to.have.length(8);
+		
+		let fileNames = entries.files.map(e => e.name);
+		exp = [
+			'zero.txt',
+			'one.txt',
+			'symlink-to-symlink-to-file',
+			'broken-symlink',
+			path.join('a', 'one.txt'),
+			path.join('a', 'symlink-to-two.txt'),
+			path.join('b', 'c', 'one.txt'),
+			path.join('b', 'c', 'two.txt'),
+			path.join('to-outside', 'symlink-to-outside-file'),
+		];
+		exp.forEach(x => {
+			expect(fileNames).to.contain(x);
+		});
+		expect(entries.files).to.have.length(exp.length);
+		
+		expect(entries).to.have.length(entries.files.length + entries.folders.length);
+	});
+
+	
 });
 
